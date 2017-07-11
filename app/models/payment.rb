019d61d008d17bd23277c5c1e4411685156cfcb6
Otavio Augusto <tavioalves@gmail.com>
2017-07-11 16:53:22 -0300

    Adição de stripe para pagamento dos usuarios

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,10 +1,10 @@
-class Payment < ApplicationRecord
+class Payment < ActiveRecord::Base
 
 	attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
 	belongs_to :user
 
 	def self.month_options
-		Date::MONTHNAMES.compact.each_with_index.map { | name, i | ["#{i+1} - #{name}", i+1]}
+		Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1]}
 	end
 
 	def self.year_options
@@ -12,12 +12,11 @@ class Payment < ApplicationRecord
 	end
 
 	def process_payment
-		customer = Stripe::costumer.create email: email, card: token
+		customer = Stripe::Customer.create email: email, card: token
 
-		Stripe::Charge.create customer: costumer.id,
-							  amout: 1000,
+		Stripe::Charge.create customer: customer.id,
+							  amount: 1000,
 							  description: 'Premium',
 							  currency: 'usd'
 	end
-
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,3 @@
-Rails.configuration.stripe = {
-	:publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'], 
-	:secret_key => ENV['STRIPE_TEST_SECRET_KEY']
-}
+require "stripe"
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = "pk_test_VsKKzJRxcdVOvY1dNhmDAYy2"
